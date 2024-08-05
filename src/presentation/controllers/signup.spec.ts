d84b86cb3e8694730b626c29d27a159395f9565e
Fernanda Nogueira <fernanda.canto.ngr@gmail.com>
2024-08-05 20:46:01 -0300

    test: ensure SignupController returns 400 if no email provider

--- src/presentation/controllers/signup.spec.ts
+++ src/presentation/controllers/signup.spec.ts
@@ -4,13 +4,30 @@ describe('SignUp controller', () => {
   test('Should return 400 if no name is provided', () => {
     const sut = new SignUpController()
     const httpRequest = {
-      email: 'test@test.com',
-      password: '1234',
-      passwordConfirmation: '1234'
+      body: {
+        email: 'test@test.com',
+        password: '1234',
+        passwordConfirmation: '1234'
+      }
     }
 
     const httpResponse = sut.handle(httpRequest)
     expect(httpResponse.statusCode).toBe(400)
     expect(httpResponse.body).toEqual(new Error('Missing param: name'))
+  })
+
+  test('Should return 400 if no email is provided', () => {
+    const sut = new SignUpController()
+    const httpRequest = {
+      body: {
+        name: 'any name',
+        password: '1234',
+        passwordConfirmation: '1234'
+      }
+    }
+
+    const httpResponse = sut.handle(httpRequest)
+    expect(httpResponse.statusCode).toBe(400)
+    expect(httpResponse.body).toEqual(new Error('Missing param: email'))
   })
 })
